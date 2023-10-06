3f3b5d48c61a139ce9b48bed2b828375815b2611
HongjiChou777 <116174245+HongjiChou777@users.noreply.github.com>
2023-10-06 09:15:47 +0000

    2023/20/06/17点16分     lines_sort     test5-7

---
+++ C programming Language/Chapter_5/lines_sort.C
@@ -0,0 +1,163 @@
+# include <stdio.h>
+# include <string.h>
+# define MAXLINES 5000
+
+char *lineptr[MAXLINES];
+#define MAXLEN 1000
+char *alloc(int);
+char *alloc(int n);
+int readlines(char *lineptr[], int maxlines);
+
+void afree(char *p);
+void qsort(char *v[], int left, int right);
+void writelines(char *lineptr[], int nlines);
+void qsort(char *v[], int left, int right);
+
+
+
+int main(void)
+{
+    int nlines;  //读取输入行的数目
+    printf("IN THE FUNCATION!\n");  //find error
+    if ((nlines = readlines(lineptr, MAXLINES)) >= 0)
+    {
+        printf("nlines >= 0\n");    //find error
+
+        qsort(lineptr, 0, nlines-1);
+
+        printf("qsort FUNCATION done!\n");    //find error
+
+
+        writelines(lineptr, nlines);
+        printf("Finish.");
+        return 0;
+    }
+    else
+    {
+        printf("error: input too big to sort\n");
+        return 1;
+    }
+       
+}
+
+
+int readlines(char *lineptr[], int maxlines)
+{
+    int getline(char s[], int lim);
+    int len, nlines;
+    char *p;
+    char line[MAXLEN];
+    nlines = 0;
+
+    while ((len = getline(line, MAXLEN)) > 0)
+    {
+        if(nlines >= maxlines || (p = alloc(len)) == NULL) //new line超过上限，或分配的内存空间不足
+        {
+            return -1;
+        }
+        else
+        {
+            line[len -1] = '\0';    //删除换行符
+            strcpy(p, line);        //将line，就是现在这行的内容拷贝到刚刚分配的内存空间p中
+            lineptr[nlines++] = p;  //将p保存到行指针中
+        }
+        
+    }
+    return nlines;  //将读取到的行的数目返回给主函数
+
+
+   
+}
+
+void writelines(char *lineptr[], int nlines)
+{
+    int i;
+    for ( i = 0; i < nlines; i++)
+    {
+        printf("%s\n",lineptr[i]);  //error:忘记写数组下标i
+    }
+    
+}
+
+int getline(char s[], int lim)
+{
+    int i, c;
+    for (i = 0; i < lim-1 && (c=getchar()) != EOF && c != '\n'; i++)
+    {
+        s[i] = c;
+    }
+    
+    if (c == '\n')
+    {
+        s[i++] = c;
+    }
+    s[i] = '\0';
+    return i;
+
+}
+
+
+
+//def alloc and afree
+#define ALLOCSIZE 10000 
+
+static char allocbuf[ALLOCSIZE];
+static char *allocp = allocbuf;
+
+char *alloc(int n)
+{
+    if (allocbuf + ALLOCSIZE - allocp >= n) //首地址 + 总大小 - 已分配单元的最后一个单元地址 >= 需要空间 
+    {
+        allocp += n;        //先加n保存变量
+        return allocp - n;  //再减n返回所需地址,(该函数返回char *变量;)
+    }
+    else
+    {
+        return 0;
+    }
+    
+}
+
+void afree(char *p)
+{
+    if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
+    {
+        allocp = p;
+    }
+    
+}
+
+void qsort(char *v[], int left, int right)
+{
+    void swap_my(char *s[], int i, int j);
+    int i, last;
+    if (left >= right)  //find error 没有写结束判定语句；
+    {
+        return;
+    }
+    
+    swap_my(v, left, (left + right)/2);
+    last = left;
+    for ( i = left+1; i <= right; i++)
+    {
+        if (strcmp(v[i], v[left]) < 0)
+        {
+            swap_my(v, ++last, i);  //find error:i写成left；
+        }
+        
+    }
+    swap_my(v, last, left);
+
+    qsort(v, left, last-1);
+    qsort(v, last+1, right);
+    
+    
+}
+
+void swap_my(char *s[], int i, int j)
+{
+    int temp;
+    temp = *s[i];
+    *s[i] = *s[j];
+    *s[j] = temp;
+}
